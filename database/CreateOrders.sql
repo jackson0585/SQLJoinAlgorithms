9254ddd14e66fe4277ab838a37e4dedc388b15a1
jackson0585 <343716061@qq.com>
2017-07-12 19:05:24 +0800

    Add SQL scripts

---
+++ database/CreateOrders.sql
@@ -0,0 +1,22 @@
+USE [SQLJoinAlgorithms]
+GO
+
+/****** Object:  Table [dbo].[Orders]    Script Date: 07/06/2017 07:18:00 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[Orders](
+	[OrderID] [int] NOT NULL,
+	[CustomerID] [nvarchar](50) NOT NULL,
+ CONSTRAINT [PK_Orders] PRIMARY KEY CLUSTERED 
+(
+	[OrderID] ASC
+)WITH (PAD_INDEX  = OFF, STATISTICS_NORECOMPUTE  = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS  = ON, ALLOW_PAGE_LOCKS  = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+
